19273449db2fea80769f97df8493fec37643738f
arqtiq <gratien.vernier@gmail.com>
2016-04-26 15:34:01 +0200

    updated main and project file

--- main.cpp
+++ main.cpp
@@ -7,9 +7,17 @@ using namespace std;
 int main()
 {
     if(!RemotePlayer::Instance()->Init())
+    {
         std::cout << "Error Initializing RemotePlayer" << std::endl;
+        return 0;
+    }
     else
         std::cout << "RemotePlayer Initialized !" << std::endl;
+
+    while(true)
+    {
+    	RemotePlayer::Instance()->Update();
+    }
 
     return 0;
 }
